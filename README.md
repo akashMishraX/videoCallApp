0b7b07df9a9fcab09067cece36a4cc099d4ca255
Akash mishra <akashmishraX1@gmail.com>
2024-12-21 18:15:23 +0530

    past commit

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # <- v0 ->
 ## <-- v0.0 -->Foundational
-    1. Set up a Progessive Web App
+    1. Set up a Progessive Web App 
     2. create a simple socket connection and test it on terminal
-    3. Documentations ----> create whole plan like project doc ,db schema and wireframe of the project is made.
+    3. Documentations  
+        - create whole plan like project doc ,db schema and wireframe of the project is made.
 
 ## <-- v0.1 -->
     3. Create a  client message component frontend using vuejs and using it instead of terminal
--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'My Basic PWA',
+        short_name: 'BasicPWA',
+        description: 'A basic starter PWA with Vue 3 and Vite',
+        theme_color: '#ffffff',
+      },
+    }),
+  ],
+});
