a21354856ca3943024920019d372d2511c483803
Akash mishra <akashmishraX1@gmail.com>
2024-12-26 09:50:14 +0530

    Backend server is updated

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,16 +1,23 @@
 import express,{ Request, Response} from "express"
-import { createServer, Server } from "node:http"
+import { createServer} from "node:http"
 import  setupSocket  from "./sockets/socketSetup";
+import  cors  from 'cors'
+
 
 
 const app = express()
 const httpServer = createServer(app)
 const port = process.env.PORT || 3000;
-
+console.log(process.env.PORT)
 const io = setupSocket(httpServer,2)
 
+app.use(cors())
+app.use(express.json())
+
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('<h1>Hello world</h1>')
+  res.send(`<h1>⚡️[server]: Server is running</h1>\n
+            <h1>⚡️[client]: Client is running on <a href="http://localhost:5173">5173</a></h1>`)
 })
 
 
@@ -18,3 +25,4 @@ app.get('/', (req: Request, res: Response) => {
 httpServer.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
 })
+
